618bd6ee09e4be89504bd77c62215d532fea9fd3
Insop Song <insop.song@gmail.com>
2025-06-05 19:13:31 +0000

    cleanup wip code

--- src/evaluations/eval_coir.py
+++ src/evaluations/eval_coir.py
@@ -7,12 +7,6 @@ import numpy as np
 import torch
 from utils import Retriever
 
-"""
--    # model_name = "nomic-ai/CodeRankEmbed"
--    model_name = "/workspace/Embeddings/contrastors/src/contrastors/ckpts/nomic-embed-text-v1-og-all_w0/step_4000/model_hf"
--    # model_name = "Snowflake/snowflake-arctic-embed-m-v1.5"
--    # model_name = "/workspace/Embeddings/contrastors/src/contrastors/ckpts/nomic-embed-text-v1-og-all/final_model_hf"
-"""
 #ran with 512 seq length for fair comparison against baselines following CoIR paper although higher seq length may yield better pfm
 def main(model_name: str, tasks: str = 'all', output_dir: str = 'results', batch_size: int = 256, max_seq_length: int = 512, multiprocess: bool = True):
     print(f"Using model: {model_name}")
